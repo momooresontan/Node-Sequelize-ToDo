fc2af6fc876294b109429a408732a4fb757716db
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-01 16:47:05 +0100

    todo route created

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
---
+++ routes/todoRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/todos", (req, res) => {});
+router.post("/todos", (req, res) => {});
+router.get("/todo/:id", (req, res) => {});
+router.put("/todos", (req, res) => {});
+router.delete("/todos", (req, res) => {});
