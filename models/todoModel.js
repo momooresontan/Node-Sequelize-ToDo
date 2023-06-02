ada3112161c63ab5ed0d08e7dcde10197aedf366
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-02 02:47:31 +0100

    todo model modified

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,1 +1,19 @@
-const {} = require("../db");
+const { sequelize } = require("../db");
+const { DataTypes } = require("sequelize");
+
+sequelize.define("Task", {
+  content: {
+    type: DataTypes.STRING,
+    validate: {
+      max: 150,
+    },
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  is_complete: {
+    type: DataTypes.BOOLEAN,
+  },
+});
+
+sequelize.sync();
